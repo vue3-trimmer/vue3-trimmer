1fa10265bf5552164d48f544f2898d7c38730067
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2023-10-01 04:44:43 +0300

    init

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
 import VTrimmer from './VTrimmer.vue'
-import VFrameVue from './VFrame.vue'
 
-export { VTrimmer, VFrameVue }
+export { VTrimmer }
--- vendor.d.ts
+++ vendor.d.ts
@@ -1,3 +1,1 @@
-import { Button } from "./dist/src/"
-
 declare module 'vue3-trimmer';
